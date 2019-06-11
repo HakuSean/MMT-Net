66e23b8c23b9592465c309c61a88b612920eac15
Dongang <dawnwang1121@gmail.com>
2019-06-11 11:41:12 +1000

    remove load_volume and registration

--- ctdataset.py
+++ ctdataset.py
@@ -10,6 +10,7 @@ import random
 import nibabel as nib
 import SimpleITK as sitk
 
+
 class CTRecord(object):
     def __init__(self, row):
         self._data = row
@@ -43,17 +44,17 @@ class CTDataSet(data.Dataset):
         self.temporal_transform = temporal_transform
         self.num_classes = 2 # dataset feature useful for model
         self.image_type = image_type
-        self.registration = registration # default is False, can be the registration method
+        self.registration = registration
 
         if image_type == 'jpg':
             self.image_tmpl = 'img{}_{:05d}.jpg'
 
         self._parse_list()
 
-    def _load_volume(self, record):
+    def _load_images(self, directory, indices=None):
         '''
         input:
-            CTRecord of the study.
+            directory of the study, or file name of nifti.
             indices: indices of the frames selected from temporal_transform
 
         output: list of imgs, length depends on self.num_channels
@@ -63,22 +64,18 @@ class CTDataSet(data.Dataset):
         Operations:
             if thickness > 1: output the average image of multiple consecutive images
             if image_type is dicom: need to conduct slope and intercept to all images
-
-        Usage:
-            1. if indices is None, then load all images and output.
-            2. if indices is not None, which means the input size of the network is given,
-                then load all images to memory and select values from the np.array.
-
         '''
+        samples = list()
         if self.image_type == 'jpg':
-            # read all images
-            volume = np.array([np.array(Image.open(os.path.join(record.directory, self.image_tmpl.format(0, i)))) for i in range(record.num_slices)])
+            for idx in indices:
+                imgs = np.array([np.array(Image.open(os.path.join(directory, self.image_tmpl.format(0, idx + i)))) for i in range(self.sample_thickness)])
+                samples.append(Image.fromarray(imgs.mean(axis=0).astype('float32')))
 
         elif self.image_type in set(['dcm', 'dicom']):
             # need to read dicoms and conduct windows, this is mainly designed for testing
 
             reader = sitk.ImageSeriesReader()
-            dicom_names = reader.GetGDCMSeriesFileNames(next(os.walk(record.directory, topdown=False))[0])
+            dicom_names = reader.GetGDCMSeriesFileNames(next(os.walk(folder, topdown=False))[0])
 
             # for windows, needs to read one image and get metadata.
             # 0028,1050 -- Window Center
@@ -92,28 +89,26 @@ class CTDataSet(data.Dataset):
             rescaleIntercept = int(single_reader.GetMetaData('0028|1052'))
             rescaleSlope = int(single_reader.GetMetaData('0028|1053'))
  
-            # change image pixel values
+             # change image pixel values
             yMin = winCenter - 0.5 * winWidth
             yMax = winCenter + 0.5 * winWidth
 
-            # read in all images
-            reader.SetFileNames(dicom_names)
-            volume = sitk.GetArrayFromImage(reader.Execute())
-            volume = np.clip(imgs * rescaleSlope - rescaleIntercept, yMin, yMax)
+            for idx in indices:
+                reader.SetFileNames(dicom_names[idx:idx + self.sample_thickness])
+                imgs = sitk.GetArrayFromImage(reader.Execute()).mean(axis=0)
+                # conduct rescale and window
+                imgs = np.clip(imgs * rescaleSlope - rescaleIntercept, yMin, yMax)
+                samples.append(Image.fromarray(imgs.astype('float32')))
 
         elif self.image_type in set(['nifti', 'nii', 'nii.gz']):
-            reader = sitk.ReadImage(record.directory+'.nii.gz') # directory is actually the file name of nii's
+            reader = sitk.ReadImage(directory+'.nii.gz') # directory is actually the file name of nii's
             volume = sitk.GetArrayFromImage(reader)
 
-        return volume # numpy.ndarray
-
-    def _load_images(self, volume, indices):
-        samples = list()
-        for idx in indices:
-            imgs = volume[idx: idx + self.sample_thickness]
-            samples.append(Image.fromarray(imgs.mean(axis=0).astype('float32')))
-        return samples # list(Images)
-
+            for idx in indices:
+                imgs = volume[range(idx, idx + self.sample_thickness)]
+                samples.append(Image.fromarray(imgs.mean(axis=0).astype('float32')))
+            
+        return samples
         # if not second_offset:
         #     y_img = Image.open(os.path.join(directory, self.image_tmpl.format(1, idx))).convert('L')
         # else:
@@ -139,13 +134,11 @@ class CTDataSet(data.Dataset):
 
     def __getitem__(self, index):
         record = self.ct_list[index]
-        segment_indices = self.temporal_transform(record)
-        
-        volume = self._load_volume(record)
         if self.registration:
-            volume = self.registration(volume)
-            
-        images = self._load_images(volume, segment_indices)
+            images = self._load_images
+
+        segment_indices = self.temporal_transform(record)
+        images = self._load_images(record.path, segment_indices)
 
         return self.transform(images), record.label
 

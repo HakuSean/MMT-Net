dc46a58a654ecc8f0ae9edc8fbcc7f02f2d0e732
Dongang Wang <dongang.wang@snac.com.au>
2019-07-01 10:44:12 +1000

    rename test.py to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,5 @@
 '''
-python3 ensemble.py ct40k_0514_bin_hemorrhage --model_depth 18 --n_test_samples 1 --sample_duration 20 --n_classes 2 --sample_size 384 --resume_path ./results/ct40k_0514_bin_hemorrhage_b4_s384_d20_randomcrop_flip_rot30/best_20.pth 
+python3 evaluate.py ct40k_0514_bin_hemorrhage --model_depth 18 --n_test_samples 1 --sample_duration 20 --n_classes 2 --sample_size 384 --resume_path ./results/ct40k_0514_bin_hemorrhage_b4_s384_d20_randomcrop_flip_rot30/best_20.pth 
 
 '''
 
@@ -18,7 +18,7 @@ from torchvision import transforms
 
 from utils import *
 from ctdataset import CTDataSet
-from epochs import test_epoch
+from epochs import evaluate_model
 from opts import parse_opts
 from model3d import generate_3d
 from tsnmodel import generate_tsn
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         os.environ["CUDA_VISIBLE_DEVICES"] = gpus
 
     # input label files
-    test_list = os.path.join(args.annotation_path, args.dataset, 'validation.txt')
+    eval_list = os.path.join(args.annotation_path, args.dataset, 'validation.txt')
 
     # set directory to save logs and training outputs
     if args.tag:
@@ -57,11 +57,11 @@ if __name__ == '__main__':
         os.makedirs(outpath)
     
     # set name of logs
-    test_logger = create_logger(outpath, 'test')
-    test_logger.info('Using model {}'.format(args.test_models))
-    test_logger.info('Using weights {}'.format(score_weights))
+    eval_logger = create_logger(outpath, 'eval')
+    eval_logger.info('Using model {}'.format(args.test_models))
+    eval_logger.info('Using weights {}'.format(score_weights))
 
-    test_logger.info('Initial Definition time: {}'.format(time.time() - start))
+    eval_logger.info('Initial Definition time: {}'.format(time.time() - start))
 
     score_lists = list()
 
@@ -118,9 +118,9 @@ if __name__ == '__main__':
         ])
         temporal_transform = TemporalSegmentCrop(snap_opts.n_slices, snap_opts.sample_thickness, test=True)
 
-        test_data = CTDataSet(test_list, snap_opts.sample_thickness, snap_opts.input_format, spatial_transform, temporal_transform, snap_opts.registration)
-        test_loader = torch.utils.data.DataLoader(
-            test_data,
+        eval_data = CTDataSet(eval_list, snap_opts.sample_thickness, snap_opts.input_format, spatial_transform, temporal_transform, snap_opts.registration)
+        eval_loader = torch.utils.data.DataLoader(
+            eval_data,
             batch_size=1,
             shuffle=False,
             num_workers=args.n_threads,
@@ -128,7 +128,7 @@ if __name__ == '__main__':
 
         print('Preparation time for {}-{} is {}'.format(arch, snap_opts.model_type, time.time() - start))
         
-        scores = test_epoch(test_loader, model, snap_opts, test_logger, concern_label=snap_opts.concern_label)
+        scores = evaluate_model(eval_loader, model, snap_opts, eval_logger, concern_label=snap_opts.concern_label)
         score_lists.append(np.array(scores))
 
         print('Prediction time for {}-{} is {}'.format(arch, snap_opts.model_type, time.time() - start))
@@ -143,30 +143,28 @@ if __name__ == '__main__':
         final_scores += w * softmax(s) # should use softmax, so the score values in different models should be comparable 
 
     # calculate accuracy
-    ground_truth = np.array([record.label for record in test_data.ct_list])
+    ground_truth = np.array([record.label for record in eval_data.ct_list])
     pred_labels = (final_scores[:, 1] >= args.threshold).astype(int)
     if not args.threshold == 0.5:
-        test_logger.info('Use >={} for label 1 (usually non-hemorrhage, i.e. negative).'.format(args.threshold))
+        eval_logger.info('Use >={} for label 1 (usually non-hemorrhage, i.e. negative).'.format(args.threshold))
 
     acc = (ground_truth == pred_labels).sum() / len(ground_truth)
     
     measures = f1_score(pred_labels, ground_truth, compute=0)
         
-    test_logger.info('Final Precision (tp/tp+fp):\t{:.3f}'.format(measures[0]))
-    test_logger.info('Final Recall (tp/tp+fn):\t{:.3f}'.format(measures[1]))
-    test_logger.info('Final F1-measure (2pr/p+r):\t{:.3f}'.format(measures[2]))
-    test_logger.info('Final Sensitivity (tp/tp+fn):\t{:.3f}'.format(measures[1]))
-    test_logger.info('Final Specificity (tn/tn+fp):\t{:.3f}'.format(measures[3]))
-    test_logger.info('Final Accuracy (tn+tp/all):\t{:.03f}%'.format(acc * 100))
-    test_logger.info("AUC Score (Test): %f" % roc_auc_score(ground_truth, final_scores[:, 1]))
+    eval_logger.info('Final Precision (tp/tp+fp):\t{:.3f}'.format(measures[0]))
+    eval_logger.info('Final Recall (tp/tp+fn):\t{:.3f}'.format(measures[1]))
+    eval_logger.info('Final F1-measure (2pr/p+r):\t{:.3f}'.format(measures[2]))
+    eval_logger.info('Final Sensitivity (tp/tp+fn):\t{:.3f}'.format(measures[1]))
+    eval_logger.info('Final Specificity (tn/tn+fp):\t{:.3f}'.format(measures[3]))
+    eval_logger.info('Final Accuracy (tn+tp/all):\t{:.03f}%'.format(acc * 100))
+    eval_logger.info("AUC Score (Test): %f" % roc_auc_score(ground_truth, final_scores[:, 1]))
 
     # -----------------------------------
     # --- Analysis Results --------------
     # -----------------------------------
 
     # prepare false_alarm and missed cases
-
-
     if args.analysis:
         false_alarm = ['line\tid\t\tscores']
         missed = ['line\tid\t\tscores']
@@ -174,17 +172,17 @@ if __name__ == '__main__':
         for i in range(len(labels)):
             if labels[i] and not pred[i]: # 1 = non, 0 = hemo
                 false_alarm.append('{}\t{}\t[{:.4f} {:.4f}]'.format(
-                    i+1, test_data.ct_list[i].path.split('/')[-1], final_scores[i][0], final_scores[i][1]))
+                    i+1, eval_data.ct_list[i].path.split('/')[-1], final_scores[i][0], final_scores[i][1]))
 
             if not labels[i] and pred[i]:
                 missed.append('{}\t{}\t[{:.4f} {:.4f}]'.format(
-                    i+1, test_data.ct_list[i].path.split('/')[-1], final_scores[i][0], final_scores[i][1]))
+                    i+1, eval_data.ct_list[i].path.split('/')[-1], final_scores[i][0], final_scores[i][1]))
 
         # print
-        test_logger.info('\nFalse alarms: ')
+        eval_logger.info('\nFalse alarms: ')
         for i in false_alarm:
-            test_logger.info(i)
+            eval_logger.info(i)
 
-        test_logger.info('\nMissed cases: ')
+        eval_logger.info('\nMissed cases: ')
         for i in missed:
-            test_logger.info(i)
+            eval_logger.info(i)

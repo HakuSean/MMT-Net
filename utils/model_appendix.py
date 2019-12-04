e9b14e99e1c673c80f91eac384dcc1a3e21734ab
Dongang Wang <dongang.wang@snac.com.au>
2019-12-04 12:18:37 +1100

    [ADD] add iterable data prefetcher for faster data loading. (not saving time for now)

--- utils/model_appendix.py
+++ utils/model_appendix.py
@@ -120,3 +120,41 @@ class ChannelSELayer3D(nn.Module):
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1, 1)
         return x * y.expand_as(x)
+
+
+# data prefetcher
+class data_prefetcher():
+    def __init__(self, loader):
+        self.loader = iter(loader)
+        self.stream = torch.cuda.Stream()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.next_input, self.next_target = next(self.loader)
+        except StopIteration:
+            self.next_input = None
+            self.next_target = None
+            return
+
+        with torch.cuda.stream(self.stream):
+            self.next_input = self.next_input.cuda(non_blocking=True)
+            self.next_target = self.next_target.cuda(non_blocking=True)
+            self.next_input = self.next_input.float()
+            
+    def __next__(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        input = self.next_input
+        target = self.next_target
+        if input is not None:
+            input.record_stream(torch.cuda.current_stream())
+        if target is not None:
+            target.record_stream(torch.cuda.current_stream())
+        self.preload()
+        return input, target
+
+    def __iter__(self):
+        return self
+
+
+
